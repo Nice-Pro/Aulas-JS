360e8ad3f018c7d644f7ce0b07f62115e5556176
Fabiano <fabiano_fabianinho@hotmail.com>
2021-08-01 17:05:22 -0300

    Introdução ao JS

---
+++ main.js
@@ -0,0 +1,63 @@
+//loop e condicionais
+
+/*
+var idade = prompt("Qual é sua idade?")
+//var idade = 18;
+if(idade > 18){
+    alert("maior de idade")
+}else{
+    alert("menor de idade")
+};
+*/
+
+var d = new Date();
+//alert(d);
+//alert(d.getMonth()+1);
+//alert(d.getMinutes());
+alert(d.getDay());
+
+/*
+var count;
+for(count = 0; count <=5;count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 5;
+while(count < 5){
+    console.log(count);
+    //count = count + 1;
+    //alert(count);
+    count++;
+}*/
+
+//variaveis e concactenação
+
+//var nome = "Fabiano Rissardi";
+//var idade = 25;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+// alert(nome + "tem"+ idade + "anos" );
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("japão","Brasil"));
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+
+//Array e dicionário
+
+//var lista = ["maçã","pera","laranja"];
+// coloca lista.push("Banana");
+// tira lista.pop("pera");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" X "));
+
+//var fruta = {nome:"maçã",cor:"vermelho"};
+//var frutas = [{nome:"maçã", cor:"vermelho"}, {nome:"uva",cor:"vinho"}];
+//console.log(fruta.nome);
+//alert(fruta.cor);
+//console.log(frutas);
+//alert(frutas[1].nome);
