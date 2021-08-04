98de92d8403fc765f2d66b3b683239ded867da8e
Fabiano <fabiano_fabianinho@hotmail.com>
2021-08-03 23:06:15 -0300

    Praticando JS

---
+++ exercicios-JS/exercicios.js
@@ -0,0 +1,23 @@
+/* 
+let line = gets().split("");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B;
+console.log("X = " + total);
+*/
+
+/*
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let total = valo1 * valor2;
+console.log("PROD = " + total);
+*/
+
+/*
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+let salary = parseFloat(valor2 * valor3).toFixed(2);
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);
+*/
